c08b51a7bc1d09b8558c7f59508314cdfed111a5
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2024-01-08 13:01:30 +0300

    Add configuration options and update login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,8 @@
 describe('recipe project', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.viewport(1200, 1200);
+})
   it('login', () => {
     cy.visit('http://localhost:3000/')
     cy.url().should('include', '/')
@@ -6,6 +10,7 @@ describe('recipe project', () => {
     cy.get('[data-test="loginName"]').should('be.visible').type("asp")
     cy.get('[data-test="loginPassword"]').should('be.visible').type("Clarusway7")
     cy.get('[data-test="loginSbmt"]').should('be.visible').click({force: true})
+    cy.url().should('include', '/home')
   })
  
 })
