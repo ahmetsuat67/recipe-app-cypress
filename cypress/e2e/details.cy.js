a66aef19cba6dbcdd2b590a130101ebe5c12ab24
ahmetsuat67 <93923638+ahmetsuat67@users.noreply.github.com>
2024-01-08 14:19:17 +0300

    Update home.cy.js to click the first card button and verify URL

--- cypress/e2e/details.cy.js
+++ cypress/e2e/details.cy.js
@@ -5,12 +5,6 @@ describe('recipe project', () => {
     })
     it('login', () => {
         cy.login()
-        cy.get("[data-test='homeAbout']").contains("About").should("be.visible").click({ force: true });
-        cy.url().should("include", "/about");
-        cy.get("[data-test='homeLogo']").contains("<Clarusway/>").should("be.visible").click({ force: true });
-        cy.url().should("include", "/home");
-        cy.get("[data-test='recipeHeader']").contains("Welcome to my").should("be.visible")
-        cy.get("[data-test='recipeSubHeader']").contains("Food App").should("be.visible")
         cy.get("[data-test='homeSearch']").type("r")
         cy.get("[data-test='homeSearchBtn']").click({ force: true });
         cy.get("[data-test='cardHeader']").should("be.visible")
